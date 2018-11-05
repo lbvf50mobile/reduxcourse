e06492c3937a9006ba3dcd32d1561dec953d6396
Dmitry <lbvf50.mobile@gmail.com>
2018-11-06 00:14:11 +0700

    Make react work

--- redux/reduxcode.js
+++ redux/reduxcode.js
@@ -1,35 +1,18 @@
 'use strict';
 
+// https://www.npmjs.com/package/react-dom
+// https://www.npmjs.com/package/react
+// https://github.com/mapbox/node-sqlite3/issues/968#issuecomment-399702443
+
 let counter = require('./reducer_counter');
+var React = require('react');
+var ReactDOM = require('react-dom')
 import { createStore } from 'redux'
-//---------------------------------------------
-// CreateStore self implementation
-//---------------------------------------------
-const createStore1 = (reducer) =>{
-    let state;
-    let listeners = [];
-    const getState = () => state;
 
-    const dispatch = (action) =>{
-        state = reducer(state, action);
-        listeners.forEach(listener => listener());
-    }
+console.log(React)
+console.log(ReactDOM)
 
-    const subscribe = (listener) => {
-        listeners.push(listener);
-        return () => {
-            listeners = listeners.filter( l => l !== listener);
-        };
-    }
-
-    dispatch({});
-
-    return {getState, dispatch, subscribe};
-}
-
-//---------------------------------------------
-
-let store = createStore1(counter)
+let store = createStore(counter)
 let render = () => {
     document.body.innerText = store.getState(); 
 }
